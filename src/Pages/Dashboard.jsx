bc40c87151781e64977c3ed9fcba3d000cfb786c
dean <deanraguso@gmail.com>
2021-07-14 13:59:45 +1000

    Removed: MaterialUI and installed Bootstrap.

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,13 +1,18 @@
 export default function Dashboard(){
   return(
-    <>
-      <p>Data Required</p>
-      <ul>
-          <li>current user data - all</li>
-          <li>associated projects for user</li>
-          <li>associated tickets for the user</li>
-          <li>potentially notifications or updates</li>
-      </ul>
-    </>
+    <div className="d-flex flex-wrap">
+      <div className="quart_chunk">
+        <p>Data Required</p>
+        <ul>
+            <li>current user data - all</li>
+            <li>associated projects for user</li>
+            <li>associated tickets for the user</li>
+            <li>potentially notifications or updates</li>
+        </ul>
+      </div>
+      <div className="quart_chunk"></div>
+      <div className="quart_chunk"></div>
+      <div className="quart_chunk"></div>
+    </div>
   )
 }
