6776b48ed38ac19691afbc426b231b6e9ca0e404
dean <deanraguso@gmail.com>
2021-07-25 21:42:56 +1000

    Created: Tests for each feature of the landing page.

--- cypress/integration/landing_spec.js
+++ cypress/integration/landing_spec.js
@@ -20,8 +20,69 @@ describe("On the Landing page", () => {
                 // Logs the user in.
                 cy.contains("a", "Log in Client")
                     .click()
+                
+                // User hits the dashboard.
+                cy.contains("h2", "Account")
+                    .should('be.visible')
             });
         })
-        
     })
+
+    describe('clicking the create an account button', () => {
+        it('opens a modal', () => {
+
+            // Loads the page and clicks the create button
+            cy.visit("https://lil-bugga.netlify.app/")
+            cy.contains("button", "Create an Account!")
+                .click()
+
+            // Fill in the form and click create.
+            cy.contains("div", "Create Account").should('be.visible')
+            cy.get("input").eq(2)
+                .type("Test User")
+            cy.get("input").eq(3)
+                .type("test@user.com")
+            cy.get("input").eq(4)
+                .type("password")
+            cy.get("input").eq(5)
+                .type("password")
+                
+            // Should be filled out and ready to submit.
+            cy.get("input").eq(2)
+                .should("have.value", "Test User")
+            cy.get("input").eq(3)
+                .should("have.value", "test@user.com")
+            cy.get("input").eq(4)
+                .should("have.value", "password")
+            cy.get("input").eq(5)
+                .should("have.value", "password")
+        });
+    })
+
+    describe('filling in the login form', () => {
+        it('should upon clicking the login button, redirect to the dashboard.', () => {
+            
+            // Go back to the landing.
+            cy.visit("https://lil-bugga.netlify.app/")
+
+            // Fill in login and click login
+            cy.get("input").eq(0)
+                .type("sample@sample.com")
+            cy.get("input").eq(1)
+                .type("password")
+            cy.contains("button", "Login")
+                .click()
+
+            // Check you are on the dashboard page
+            cy.contains("h2", "Account")
+                .should('be.visible')
+
+            // Sign out
+            cy.get("a").eq(3)
+                .click()
+
+        });
+    })
+    
+    
 })
